a5a6d38d3e45f14818dd85ad5689ec10d58528c4
Krzysztof Garczynski <krzysztof.garczynski@gmail.com>
2024-10-11 11:24:09 +0200

    Updated nvim options

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,18 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+
+o.list = true
+o.listchars = "tab:>-,trail:-"
+o.foldenable = false
+o.smarttab = true
+o.wildignore = "*.swp,*.bak,*.pyc,*.class,*.o,*.obj"
+
+o.linespace = 0
+o.report = 0
+o.ruler = true
+o.signcolumn = "yes"
+o.conceallevel = 2
+o.colorcolumn = "80"
