b32b5a97ab81ce0eb42111efc92e91f4223b90e4
Krzysztof Garczynski <krzysztof.garczynski@gmail.com>
2025-07-24 18:16:30 +0200

    Updated NvChad plugins
    Signed-off-by: Krzysztof Garczynski <krzysztof.garczynski@gmail.com>

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,13 +1,10 @@
--- load defaults i.e lua_lsp
-require("nvchad.configs.lspconfig").defaults()
-
+local nvlsp = require "nvchad.configs.lspconfig"
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
-local servers = { "clangd", "pylsp" }
-local nvlsp = require "nvchad.configs.lspconfig"
+nvlsp.defaults()
 
--- lsps with default config
+local servers = { "clangd", "pylsp" }
+
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
@@ -16,9 +13,3 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
